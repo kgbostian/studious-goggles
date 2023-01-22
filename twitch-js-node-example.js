57220ffb183c55d8714a62ef388e4f27e39874c4
Kashimoto <kashimoto503@gmail.com>
2023-01-22 09:20:41 -0700

    Removal of unneeded items.

--- twitch-js-node-example.js
+++ twitch-js-node-example.js
@@ -6,14 +6,38 @@ const username = process.env.USERNAME;
 const token = process.env.TOKEN;
 const channel = process.env.CHANNEL;
 
-const run = async () => {
-  const chat = new Chat({
-    username,
-    token
-  });
+const express = require('express')
+const app = express()
+const port = 13455
+var cors = require('cors')
+const bodyParser = require('body-parser');
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    console.log('Got body:', req.body);
+    respondChat(req.body);
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)  
+})
+
+const chat = new Chat({
+  username,
+  token
+});
+
+function respondChat(diceValue) {
+  chat.say(channel,'Result was ' + diceValue);
+}
+const run = async () => {
   await chat.connect();
   await chat.join(channel);
+
 
   // chat.on('PRIVMSG', message => {
   //   // Do stuff ...
@@ -21,51 +45,6 @@ const run = async () => {
   //   chat.say(channel, 'Kappa Keepo Kappa')
   // });
 
-  const express = require('express')
-  const app = express()
-  const port = 13455
-  var cors = require('cors')
-  const bodyParser = require('body-parser');
-
-  app.use(cors());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-    console.log("Got a get");
-  })
-
-  app.post('/', (req, res) => {
-    res.send('Got a POST request');
-    console.log("Responding to post.");
-    console.log("Got body:", req.body);
-  })
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)  
-  })
-
 };
 
 run();
-
-const express = require('express')
-const app = express()
-const port = 13455
-var cors = require('cors')
-
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  console.log("Got a get");
-})
-
-app.post('/', (req, res) => {
-    res.send('Got a POST request');
-    console.log("Responding to post");
-  })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
